8ff2bf49d29a1b4a12362c0a89f738b1279853a8
ravik122 <ravik1222@gmail.com>
2015-10-06 16:02:14 +0200

    Added admin variable to users and option to delete users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:edit, :update, :index]
+	before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
 	before_action :correct_user,   only: [:edit, :update]
 
 
@@ -23,6 +23,12 @@ class UsersController < ApplicationController
 	  else
 		  render 'new'
 	  end
+  end
+
+  def destroy
+	  User.find(params[:id]).destroy
+	  flash[:success] = "User deleted."
+	  redirect_to users_url
   end
 
   def index
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@
 User.create!(name:	"Example user",
 	     email:	"example@railstutorial.org",
 	     password:			"foobar",
-	     password_confirmation:	"foobar") 
+	     password_confirmation:	"foobar",
+	     admin:	true)
 
 
 99.times do |n|
