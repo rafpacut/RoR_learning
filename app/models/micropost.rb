6470ee7356511ca0ba33d146f77111e6989ba72f
rafpacut <rafpacut@gmail.com>
2015-10-19 19:01:38 +0200

    Creating microposts.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,10 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+
+
+
+
+
 end
