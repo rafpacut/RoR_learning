c796b4b9ff3bfedb6414b26b7139b077956eeeb5
rafpacut <rafpacut@gmail.com>
2015-10-20 11:25:50 +0200

    Image validation.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,10 +4,18 @@ class Micropost < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :picture_size
 
 
 
+private
 
+	#Validates the size of an uploaded picture
+	def picture_size
+		if picture.size > 5.megabytes
+			error.add(:picture, "should be less than 5MB")
+		end
+	end
 
 
 end
