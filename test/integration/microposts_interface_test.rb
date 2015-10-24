af27e4a67a687874baed5a7339fe8abff27d9f07
rafpacut <rafpacut@gmail.com>
2015-10-25 01:22:45 +0200

    3. test for image uploader

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -13,8 +13,10 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 		log_in_as(@user)
 		get root_path
 		assert_select 'div.pagination'
+		assert_select 'input[type=file]'
 
 		#Invalid submission
+		#
 		assert_no_difference 'Micropost.count' do
 			post microposts_path, micropost: { content: "" }
 		end
@@ -23,9 +25,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 		
 		#Valid submission
 		content = "Some content about nothing, really."
+		picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
 		assert_difference 'Micropost.count', 1 do
-			post microposts_path, micropost: { content: content }
+			post microposts_path, micropost: { content: content, picture: picture }
 		end
+		assert @user.microposts.first.picture?
+
 
 		#Delete a post
 		first_micropost = @user.microposts.paginate(page: 1).first
